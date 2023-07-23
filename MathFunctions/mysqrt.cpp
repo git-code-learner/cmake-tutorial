23444b4a5262da599cb3bcb6a323ac37547c4a41
git-learner <the-naive-user@proton.me>
2023-07-23 18:58:27 +0200

    Added library MathFunctions (non-optional).

---
+++ MathFunctions/mysqrt.cpp
@@ -0,0 +1,12 @@
+/**
+ * Calculates the sqaure root of the number.
+*/
+
+#include <cmath>
+
+float mysqrt(const float &number)
+{
+    float result{};
+    result = sqrt(number);
+    return result;
+}
